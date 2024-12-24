4d6ad0666a8c91f8c936da77d4a1f400c304e7dc
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-12-24 16:15:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker mslearn0055/simple-python-flask-app
+docker pull mslearn0055/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 mslearn0055/simple-python-flask-app
